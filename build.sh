b2255d6d07cc2e34e629a848dd04fe6c0e2ffa9b
Jacob McSwain <jacob@mcswain.dev>
2024-08-19 10:21:42 -0500

    build u-boot

--- build.sh
+++ build.sh
@@ -4,7 +4,10 @@ set -euo pipefail
 
 sudo rm -rf out
 mkdir -p out
-docker build -t kernel -f Dockerfile.kernel .
+docker build -t kernel kernel
 docker run --rm -v $(pwd)/out:/out kernel
-mkimage -C none -A arm -T script -d boot.cmd out/boot.scr
-cp boot.cmd out/boot.cmd
+
+docker build -t u-boot u-boot
+docker run --rm -v $(pwd)/out:/out u-boot
+mkimage -C none -A arm64 -T script -d u-boot/boot.cmd out/boot.scr
+cp u-boot/boot.cmd out/boot.cmd
