df0d33bf9bda493de1b5b58aa6466c472d9a2ce7
Jacob McSwain <jacob@mcswain.dev>
2024-08-24 14:27:45 -0500

    u-boot config: remove redundant env

--- u-boot/boot.existing.cmd
+++ u-boot/boot.existing.cmd
@@ -3,7 +3,6 @@ setenv load_addr "0x45000000"
 setenv fdt_addr "0x49000000"
 
 setenv bootargs console=ttyS0,115200 earlyprintk root=/dev/mmcblk0p1 rootwait usb-storage.quirks=0x2537:0x1066:u,0x2537:0x1068:u
-setenv param_spidev_spi_bus 1
 
 load mmc 0 ${fdt_addr} boot/sun50i-h616-mangopi-mcore.dtb
 fdt addr ${fdt_addr}
